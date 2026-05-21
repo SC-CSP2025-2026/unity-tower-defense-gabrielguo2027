ff65a97f5faf8d6c29532b0d19d0f9c6d9815648
Gabriel Guo <gabriel.guo@scsstudent.org>
2026-05-20 19:13:59 -0700

    feat: Completed codes for video 6, adding Enemies There is a weird error where my enemy's center at 0,0,0, isn't actually the center of my grid

---
+++ Tower Defense/Assets/Scripts/EnemyRotation.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class EnemyRotation : MonoBehaviour
+{
+    [field: SerializeField]
+    public float rotatingSpeed {get; private set;} = 1f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+{
+    transform.Rotate(0f, rotatingSpeed * Time.deltaTime, 0f);
+}
+}
+//why is it 1f
+//what is Vector3
+
